f75c35d5bee1e56c2dd93d50f1bda9bc9e73dcb9
mjmot <mjmot12345@gmail.com>
2023-11-26 10:47:53 +0100

    front

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Realizar la solicitud a la API
-  fetch('API')
+  fetch('http://localhost:8080/Peregildo/status')
       .then(response => response.json())
       .then(data => {
           // Actualizar la tabla con los datos obtenidos de la API
           const tablaResultados = document.getElementById('tablaResultados');
           const tbody = tablaResultados.querySelector('tbody');
-
           // Limpiar el contenido existente en la tabla
           tbody.innerHTML = '';
-
-          // Iterar sobre los datos y agregar filas a la tabla
-          data.forEach(item => {
-              const row = `<tr>
-                              <td>${item.metrica}</td>
-                              <td>${item.valor}</td>
+		  // Mostrar ultimo valor
+		  item = data.pop();
+		  const row = `<tr>
+                              <td>${item.id}</td>
+                              <td>${item.soil_humidity}</td>
+							  <td>${item.air_humidity}</td>
+							  <td>${item.temperature}</td>
                            </tr>`;
-              tbody.innerHTML += row;
-          });
+			
+		  tbody.innerHTML += row;
       })
       .catch(error => console.error('Error al obtener datos de la API:', error));
 });
